d4b32de3ebabc4ae75d877526f29a5a40b72c87e
mateo.salvador@ynov.com <mateo.salvador@ynov.com>
2022-12-09 20:29:54 +0100

    Only Letter+

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	hangman "serv-hangman/packages"
 	"strings"
+	"unicode"
 )
 
 type User struct {
@@ -25,6 +26,15 @@ var details = User{
 	Username:   "",
 	Difficulty: "",
 	// Display:    "Bienvenue dans le jeu du pendu, vous devez trouver le mot caché en entrant une lettre à la fois. Vous avez 10 tentatives pour trouver le mot. Bonne chance !, Si vous tentez un mot entier, vous perdez deux tentatives",
+}
+
+func IsLetter(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
@@ -54,23 +64,27 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func gameHandler(w http.ResponseWriter, r *http.Request) {
-	h.Game(strings.ToLower(r.FormValue("LetterTry")))
-	details.Username = r.FormValue("Username")
-	details.Difficulty = r.FormValue("Difficulty")
-	details.Word = h.Word
-	details.ToFind = h.ToFind
-	details.Attempts = h.Attempts
-	details.LetterTry = h.TriedLetters
-	details.LetterKnown = h.KnownLetters
-	details.Display = h.Message
-	fmt.Println("Lettre entrée ", r.FormValue("LetterTry"))
-	fmt.Println("Mot à trouver ", h.ToFind)
-	fmt.Println("Affichage ", h.Word)
-	fmt.Println("Lettres connues ", h.KnownLetters)
-	fmt.Println("Vous avez déjà tenté ", h.TriedLetters)
-	fmt.Println("Il vous reste ", h.Attempts, " tentatives")
-	for _, v := range h.TriedLetters {
-		fmt.Println(v)
+	if IsLetter(r.FormValue("LetterTry")) == true {
+		h.Game(strings.ToLower(r.FormValue("LetterTry")))
+		details.Username = r.FormValue("Username")
+		details.Difficulty = r.FormValue("Difficulty")
+		details.Word = h.Word
+		details.ToFind = h.ToFind
+		details.Attempts = h.Attempts
+		details.LetterTry = h.TriedLetters
+		details.LetterKnown = h.KnownLetters
+		details.Display = h.Message
+		fmt.Println("Lettre entrée ", r.FormValue("LetterTry"))
+		fmt.Println("Mot à trouver ", h.ToFind)
+		fmt.Println("Affichage ", h.Word)
+		fmt.Println("Lettres connues ", h.KnownLetters)
+		fmt.Println("Vous avez déjà tenté ", h.TriedLetters)
+		fmt.Println("Il vous reste ", h.Attempts, " tentatives")
+		for _, v := range h.TriedLetters {
+			fmt.Println(v)
+		}
+	} else {
+		details.Display = "Vous devez entrer une lettre"
 	}
 	//gestion html
 	tmpl1 := template.Must(template.ParseFiles("templates/game.html"))
